fad481f8e93611506689124dcc9d5ea9c4995f03
강성준 <ksjoon02@naver.com>
2023-11-08 21:21:25 +0900

    완성본

--- Parser.cpp
+++ Parser.cpp
@@ -5,7 +5,7 @@
 #include <cctype>
 #include <vector>
 
-//token type
+//token의 type들
 enum TokenType{
     IDENT, CONST, ASSIGN_OP, ADD_OP, MULT_OP, SEMI_COLON, LEFT_PAREN, RIGHT_PAREN, END_OF_FILE, ERROR
 };
@@ -16,15 +16,14 @@ struct Token{
     std::string value;
 };
 
-// 전역 변수들
+//global variables
 Token next_token;
 std::string token_string;
 std::ifstream input_file;
-int id_count, const_count, op_count;
+int id_count,const_count,op_count;
 bool error_flag, warning_flag;
 
-
-// 함수 선언
+//함수 
 void lexical();
 void program();
 void statements();
@@ -38,66 +37,62 @@ int evaluate_expression();
 int evaluate_term();
 int evaluate_factor();
 
-
-// Symbol table
+//symbol table
 std::map<std::string, int> symbol_table;
 
-bool is_ident(const std::string& token) {
+//식별자인지 확인
+bool is_ident(const std::string& token){
     if(token.empty()) return false;
-    
-    //첫번째가 letter이거나 _인지 확인
-    if(!std::isalpha(token[0]) && token[0] != '_') return false;
 
-    //나머지들 확인
-    for(size_t i = 1;i < token.length();++i){
+    if(!std::isalpha(token[0] != '_'))return false;
+
+    for(size_t i =1; i <token.length();++i){
         if(!std::isalnum(token[i])&&token[i] != '_') return false;
     }
     return true;
 }
+//상수(const)인지 확인
+bool is_const(const std::string& token){
+    if(token.empty()) return false;
 
-bool is_const(const std::string& token) {
-    if (token.empty()) return false;
-
-    // 각 character가 digit인지 확인
-    for (char ch : token) {
-        if (!std::isdigit(ch)) return false;
+    for(char ch : token){
+        if(!std::isdigit(ch)) return false;
     }
-
     return true;
-  return true;
 }
 
 void lexical() {
     char ch;
     token_string.clear();
 
-    // 공백 건너뛰기
-    while (std::isspace(input_file.peek())) input_file.get();
+    //아스키 코드 32이하는 무시
+    while(std::isspace(input_file.peek())) input_file.get();
 
-    if (input_file.eof()) {
+    //파일 끝에 오면 끝내기
+    if(input_file.eof()){
         next_token = {TokenType::END_OF_FILE, ""};
         return;
     }
 
     ch = input_file.get();
 
-    // indentifiers, keyword 확인하기
-    if (std::isalpha(ch) || ch == '_') {
+    //identifiers인지 확인하기
+    if(std::isalpha(ch) || ch == '_'){
         token_string += ch;
-        while (std::isalnum(input_file.peek()) || input_file.peek() == '_') {
+        while(std::isalnum(input_file.peek()) || input_file.peek() == '_'){
             token_string += input_file.get();
         }
         next_token = {IDENT, token_string};
     }
-    // constansts 확인
-    else if (std::isdigit(ch)) {
+    //constants인지 확인
+    else if(std::isdigit(ch)){
         token_string += ch;
-        while (std::isdigit(input_file.peek())) {
+        while(std::isdigit(input_file.peek())){
             token_string += input_file.get();
         }
         next_token = {CONST, token_string};
     }
-    // assignment op인지 확인
+    //assignment OP인지 확인
     else if (ch == ':') {
         if (input_file.peek() == '=') {
             input_file.get();
@@ -106,9 +101,10 @@ void lexical() {
             next_token = {ERROR, ":"};
         }
     }
-    // 나머지들 확인
-    else {
-        switch (ch) {
+    //나머지들인지 확인
+    else{
+        switch (ch)
+        {
             case '+': next_token = {ADD_OP, "+"}; break;
             case '-': next_token = {ADD_OP, "-"}; break;
             case '*': next_token = {MULT_OP, "*"}; break;
@@ -122,50 +118,45 @@ void lexical() {
 }
 
 void program() {
-    //a <program> consists of <statements>
+    //한 프로그램은 statements로 구성되어 있음
     statements();
-
-    // 다 확인후 endoffile
+    // 다 확인후 endoffile이 아니면 에러
     if (next_token.type != END_OF_FILE) {
         std::cerr << "Parsing error: Extra tokens after program end." << std::endl;
     }
 }
 
 void statements() {
-    // expect a statement
-    statement();
-
-    while (next_token.type == SEMI_COLON) {
-        // Consume the semi_colon
-        lexical();
-
-        // Parse the next statement
-        statement();
-    }
-
-    if (next_token.type != SEMI_COLON && next_token.type != END_OF_FILE) {
-        std::cerr << "Parsing error: Expected ';' or end of file." << std::endl;
-    }
+    do {
+        statement(); // 현재 토큰을 기반으로 statement 처리
+        if (next_token.type == SEMI_COLON) {
+            lexical(); // 세미콜론을 소비하고 다음 토큰으로 이동
+        } else if (next_token.type != END_OF_FILE) {
+            std::cerr << "Parsing error: Expected ';' after statement." << std::endl;
+            error_flag = true;
+            return; // 에러 발생 시 함수 종료
+        }
+    } while (next_token.type != END_OF_FILE && !error_flag); // 파일의 끝이거나 에러가 발생할 때까지 반복
 }
-
 void statement() {
-    id_count = const_count = op_count = 0; // Reset counts for each statement
-    error_flag = warning_flag = false; // Reset flags
+    //count변수들 초기화
+    id_count = const_count = op_count = 0;
+    //에러,경고문 플레그 초기화
+    error_flag = warning_flag = false;
 
-    if (next_token.type == IDENT) {
-        id_count++; // Increment identifier count
-        std::string ident = next_token.value;
-        lexical(); // Move to the next token
-
-        if (next_token.type == ASSIGN_OP) {
-            lexical(); // Move past the assignment operator
-            int value = evaluate_expression(); // Evaluate the expression and get the value
-            if (!error_flag) {
-                symbol_table[ident] = value; // Assign the value to the identifier in the symbol table
+    if(next_token.type == IDENT){
+        id_count++;
+        std::string ident = next_token.value; //다시 쓰기위함
+        lexical(); //다음 토큰 이동 
+        if(next_token.type == ASSIGN_OP){
+            lexical();
+            int value = evaluate_expression();
+            if(!error_flag){
+                symbol_table[ident] = value;
             }
             std::cout << ident << " := ";
             if (error_flag) {
-                std::cout << "Unknown; ";
+                std::cout << "Unknown; "; //예외 출력
             } else {
                 std::cout << value << "; ";
             }
@@ -177,83 +168,87 @@ void statement() {
             } else {
                 std::cout << "(OK)" << std::endl;
             }
-        } else {
+        } else{
             std::cerr << "Parsing error: Expected assignment operator after identifier." << std::endl;
             error_flag = true;
         }
-    } else {
+    } else{
         std::cerr << "Parsing error: Expected identifier at the beginning of the statement." << std::endl;
         error_flag = true;
     }
+
 }
-
-
+//<expression> → <term><term_tail>
 void expression() {
     term();
 
     term_tail();
 }
 
+//<term_tail> → <add_op><term><term_tail> | ε
 void term_tail() {
-    // term_tail → <add_op><term><term_tail> | ε
     if (next_token.type == ADD_OP) {
-        lexical(); // Consume the add operator
-        term(); // Parse the next term
-        term_tail(); // Look for another term_tail recursively
+        lexical(); //<add_op>해석
+        term(); 
+        term_tail(); 
     }
-    // If the next token is not an add_op, it's epsilon (empty), so we do nothing
 }
 
+//term -> <factor><factor_tail>
 void term() {
-    // term → <factor> <factor_tail>
     factor();
     factor_tail();
 }
 
+// factor_tail → <mult_op><factor><factor_tail> | ε
 void factor_tail() {
-    // factor_tail → <mult_op><factor><factor_tail> | ε
     if (next_token.type == MULT_OP) {
-        lexical(); // Consume the mult operator
-        factor(); // Parse the next factor
-        factor_tail(); // Look for another factor_tail recursively
+        lexical(); 
+        factor(); 
+        factor_tail(); 
     }
-    // If the next token is not a mult_op, it's epsilon (empty), so we do nothing
 }
 
-void factor() {
-    // factor → <left_paren><expression><right_paren> | <ident> | <const>
+//factor → <left_paren><expression><right_paren> | <ident> | <const>
+void factor(){
     if (next_token.type == LEFT_PAREN) {
-        lexical(); // Consume the left parenthesis
-        expression(); // Parse the expression inside the parentheses
+        lexical(); 
+        expression(); 
         if (next_token.type == RIGHT_PAREN) {
-            lexical(); // Consume the right parenthesis
+            lexical(); 
         } else {
             std::cerr << "Parsing error: Expected ')' after expression." << std::endl;
-            // Handle the error appropriately
+            
         }
     } else if (next_token.type == IDENT) {
-        // Increment identifier count only if it's a new identifier
+        // counter는 한번만 세기
         if (symbol_table.find(next_token.value) == symbol_table.end()) {
             id_count++;
         }
-        // If it's an identifier, look it up in the symbol table or insert it
+        
         symbol_table[next_token.value] = 0;
-        lexical(); // Consume the identifier
+        lexical(); 
     } else if (next_token.type == CONST) {
-        const_count++; // Increment constant count
-        lexical(); // Consume the constant
+        const_count++; 
+        lexical(); 
     } else {
         std::cerr << "Parsing error: Expected expression." << std::endl;
-        // Handle the error appropriately
     }
 }
 
-int evaluate_expression() {
+int evaluate_expression(){
     int value = evaluate_term();
     while (next_token.type == ADD_OP) {
         op_count++; // Increment operator count
         std::string op = next_token.value;
         lexical();
+        if (next_token.type == ADD_OP || next_token.type == MULT_OP) {
+            // 연속된 연산자 발견, 경고 표시
+            std::cout << "(Warning) 연속된 연산자 발견: " << op << next_token.value << std::endl;
+            warning_flag = true; // 경고 플래그 설정
+            // 첫 번째 연산자만 처리하고 두 번째 연산자는 무시
+            lexical(); // 두 번째 연산자를 소비
+        }
         if (op == "+") {
             value += evaluate_term();
         } else if (op == "-") {
@@ -265,7 +260,6 @@ int evaluate_expression() {
     }
     return value;
 }
-
 int evaluate_term() {
     int value = evaluate_factor();
     while (next_token.type == MULT_OP) {
@@ -283,16 +277,13 @@ int evaluate_term() {
     }
     return value;
 }
-
-
-
 int evaluate_factor() {
     int value = 0;
     if (next_token.type == LEFT_PAREN) {
-        lexical(); // Consume the left parenthesis
-        value = evaluate_expression(); // Evaluate the expression inside the parentheses
+        lexical(); 
+        value = evaluate_expression(); 
         if (next_token.type == RIGHT_PAREN) {
-            lexical(); // Consume the right parenthesis
+            lexical(); 
         } else {
             std::cerr << "Parsing error: Expected ')' after expression." << std::endl;
             error_flag = true;
@@ -301,25 +292,22 @@ int evaluate_factor() {
         if (symbol_table.find(next_token.value) == symbol_table.end()) {
             std::cerr << "Error: Undefined variable " << next_token.value << std::endl;
             error_flag = true;
-            value = 0; // Undefined variables are considered as 0
+            value = 0; 
         } else {
             value = symbol_table[next_token.value];
         }
-        id_count++; // Increment identifier count
-        lexical(); // Consume the identifier
+        id_count++; 
+        lexical(); 
     } else if (next_token.type == CONST) {
         value = std::stoi(next_token.value);
-        const_count++; // Increment constant count
-        lexical(); // Consume the constant
+        const_count++; 
+        lexical(); 
     } else {
         std::cerr << "Parsing error: Expected '(' or identifier or constant." << std::endl;
         error_flag = true;
     }
     return value;
 }
-
-
-
 int main(int argc, char* argv[]) {
     if (argc != 2) {
         std::cerr << "Usage: " << argv[0] << " <input_file>" << std::endl;
@@ -332,16 +320,12 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    // Initialize the lexical analyzer
     lexical();
 
-    // Start the parsing process
     program();
 
-    // Close the input file
     input_file.close();
 
-    // Output the symbol table
     if (!error_flag) {
         std::cout << "Result ==> ";
         for (const auto& pair : symbol_table) {
